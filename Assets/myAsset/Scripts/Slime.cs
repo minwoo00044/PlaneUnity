2d53a243b72fe9664cd584924a3454dc1d31a485
minwoo0004 <minwoo0004@naver.com>
2023-08-03 17:50:20 +0900

    rotation give up

--- Assets/myAsset/Scripts/Slime.cs
+++ Assets/myAsset/Scripts/Slime.cs
@@ -8,7 +8,6 @@ public class Slime : Enemy
     public float moveSpeed = 3f;
     public float rotationSpeed = 5f;
     public float changeDirectionInterval = 3f;
-
     private Vector3 currentDirection;
 
     void Start()
@@ -24,13 +23,7 @@ public class Slime : Enemy
         transform.Translate(currentDirection * moveSpeed * Time.deltaTime);
 
         // 몬스터가 이동 방향과 회전 방향을 모두 고려하여 회전
-        if (currentDirection != Vector3.zero)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(currentDirection, Vector3.up);
-            float step = rotationSpeed * Time.deltaTime;
-            transform.rotation = Quaternion.Euler(Vector3.zero);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, step);
-        }
+
     }
 
     IEnumerator ChangeDirectionCoroutine()
@@ -46,5 +39,9 @@ public class Slime : Enemy
     {
         // 랜덤한 방향 벡터 생성
         currentDirection = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f)).normalized;
+
+
+        transform.rotation = Quaternion.LookRotation(currentDirection);
+
     }
 }
